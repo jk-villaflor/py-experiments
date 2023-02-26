54fd04d9a35431c456f79ef26c13f73b585182c5
John-Kevin Villaflor <jvillaflor@myherohub.com>
2023-02-26 12:29:08 +0400

    tested chatbot from internet

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests as req
 
 targetUrl = 'http://dxvista.com/sandbox/api_previousresult'
 data = {"patid":"20051427618","examid":"2"}
-headers={'Accept':'*/*', 'Accept-Encoding':'gzip, deflate, br', 'Connection':'keep-alive'}
+headers={'Accept':'*/*', 'Accept-Encoding':'gzip, deflate, br'}
 response = req.post(targetUrl, data=data, json=data, headers=headers)
 
 print(response.content)
